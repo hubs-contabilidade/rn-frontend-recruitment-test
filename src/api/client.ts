395c55cb5e2ef5ad2f882611ce7d45f36ce4b7e0
MillaRany <milla_rany@hotmail.com>
2026-06-23 23:27:26 -0300

    Fix api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.PUBLIC_API_URL }),
+  link: new HttpLink({ uri: process.env.EXPO_PUBLIC_API_URI }),
   cache: new InMemoryCache(),
 })
